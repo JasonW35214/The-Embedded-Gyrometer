5a8e38ce34858a548cb54d6681ffbe902a7dee33
Akash <54092294+akashp04@users.noreply.github.com>
2023-12-21 19:38:30 -0500

    Update Conv.h

--- Embedded_Challenge/include/Conv.h
+++ Embedded_Challenge/include/Conv.h
@@ -2,21 +2,24 @@
 #include "math.h"
 #include "vector.h"
 
-#define PI acos(-1.0)
+#define PI 3.141592653589793238462643383279502884L 
 #define RADIUS 0.5
 #define dt 0.5
 
+std::vector<float> instanteousDistance[40];
+std::vector<float> velocity[40];
+
 float calculateLinearVelocity(float omega){
     float omega_mps, linearVelocity; 
-    // omega_mps = omega * (PI / 180); // IF the uints of angular velocity are in degrees/second
+    // omega_mps = omega * (PI / 180); // IF the units of angular velocity are in degrees/second
     // linearVelocity = RADIUS * omega_mps;
     linearVelocity = RADIUS * omega;
+    velocity.push_back(linearVelocity);
 
     return linearVelocity;
 }
 
 void calculateLinearDistance(float linearVelocity, float *DISTANCE){
-    std::vector<flaot> instanteousDistance[40];
     float instDist = abs(linearVelocity) * dt;  
     instantaneousDistance.push_back(instDist);
     *DISTANCE += instDistance;    
