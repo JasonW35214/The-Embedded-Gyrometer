98f60d4962d8f03e5f50e4096afd4ec97ab2c9ef
Akash <54092294+akashp04@users.noreply.github.com>
2023-12-21 21:35:56 -0500

    Update conversion.h

--- Embedded_Challenge/include/conversion.h
+++ Embedded_Challenge/include/conversion.h
@@ -1,26 +1,22 @@
 #include "mbed.h"
 #include "math.h"
-#include "vector.h"
 
 #define PI 3.141592653589793238462643383279502884L 
 #define RADIUS 0.5
 #define dt 0.5
 
-std::vector<float> instanteousDistance[40];
-std::vector<float> velocity[40];
+float averageVelocity = 0.0f;
 
 float calculateLinearVelocity(float omega){
     float omega_mps, linearVelocity; 
     // omega_mps = omega * (PI / 180); // IF the units of angular velocity are in degrees/second
     // linearVelocity = RADIUS * omega_mps;
     linearVelocity = RADIUS * omega;
-    velocity.push_back(linearVelocity);
-
+    averageVelocity += linearVelocity; 
     return linearVelocity;
 }
 
 void calculateLinearDistance(float linearVelocity, float *DISTANCE){
-    float instDist = abs(linearVelocity) * dt;  
-    instantaneousDistance.push_back(instDist);
-    *DISTANCE += instDistance;    
+    float instantaneousDistance = abs(linearVelocity) * dt;
+    *DISTANCE += instantaneousDistance;    
 }
