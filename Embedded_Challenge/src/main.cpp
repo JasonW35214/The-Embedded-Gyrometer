a70f3287ba5534beeb5df3b54176fff3af44d94a
Akash <54092294+akashp04@users.noreply.github.com>
2023-12-21 19:54:17 -0500

    Update main.cpp

--- Embedded_Challenge/src/main.cpp
+++ Embedded_Challenge/src/main.cpp
@@ -2,17 +2,11 @@
 
 Embedded Challenge Fall 2023
 
-
-Current Issues:
-- Measurement is not for 20 seconds, stops at 17.5, just like in scaling factor? connection? or 
-make while loop <= 21
-- data not getting written to file
-
 */
 
 #include "mbed.h"
 #include "drivers/LCD_DISCO_F429ZI.h" // file to interact with LCD
-#include "Conv.h"
+#include "challenge.h"
 
 // LCD Settings
 #define BACKGROUND 1
@@ -37,7 +31,8 @@ make while loop <= 21
 
 #define SCALING_FACTOR (17.5f * 0.017453292519943295769236907684886f / 1000.0f)
 
-#define FILTER_COEFFICIENT 0.1f // Adjust this value as needed
+#define FILTER_COEFFICIENT 0.1f // Adjust this value as needed. This is used as the coefficient for performing High/Low-Pass filter operations
+#define ERROR_SCALING_FACTOR 1.6
 
 // EventFlags object declaration
 EventFlags flags;
@@ -67,9 +62,7 @@ void button_pushed_cb(){
     flags.set(BUTTON_PRESSED_FLAG);
 }
 
-//sets the background layer 
-//to be visible, transparent, and
-//resets its colors to all black
+//sets the background layer to be visible, transparent, and resets its colors to all black
 void setup_background_layer(){
   lcd.SelectLayer(BACKGROUND);
   lcd.Clear(LCD_COLOR_BLACK);
@@ -79,8 +72,7 @@ void setup_background_layer(){
   lcd.SetTransparency(BACKGROUND,0x7Fu);
 }
 
-//resets the foreground layer to
-//all black
+//resets the foreground layer to black
 void setup_foreground_layer(){
     lcd.SelectLayer(FOREGROUND);
     lcd.Clear(LCD_COLOR_BLACK);
@@ -88,10 +80,11 @@ void setup_foreground_layer(){
     lcd.SetTextColor(LCD_COLOR_LIGHTGREEN);
 }
 
+//Timer function to keep track of the change in time while measuring the distance (used to end the while exactly after 20seconds).
 Ticker timer;
-int countSecs = 0;
+int secs = 0;
 void timer_cb(){
-    countSecs++;
+    secs++;
 }
 
 
@@ -165,12 +158,15 @@ int main() {
         
         //Push a button to initiate data recording for 20 seconds
         flags.wait_all(BUTTON_PRESSED_FLAG);
-        countSecs = 0;
-        i = 0;
+        
+        // This line of code prints the statement which shows that the data is being recorded after clicking the button 
+        // It can also be interpreted as another evidence that the gyroscope is enabled and started recording
         snprintf(display_buf[0], 60, "Movement in Progress");
         lcd.SelectLayer(FOREGROUND);
         lcd.DisplayStringAt(5, LINE(8), (uint8_t *)display_buf[0], CENTER_MODE);
-        while(countSecs <= 20){
+
+        
+        while(secs <= 20){
 
             flags.wait_all(DATA_READY_FLAG);
             write_buf[0] = OUT_X_L | 0x80 | 0x40;
@@ -182,24 +178,18 @@ int main() {
             raw_gx = (((uint16_t)read_buf[2]) << 8) | ((uint16_t)read_buf[1]);
             raw_gy = (((uint16_t)read_buf[4]) << 8) | ((uint16_t)read_buf[3]);
             raw_gz = (((uint16_t)read_buf[6]) << 8) | ((uint16_t)read_buf[5]);
-
+            
+            // Convert the raw data to radians per second and multiply with Gain (Sensitivity)
             gx = ((float)raw_gx) * SCALING_FACTOR;
             gy = ((float)raw_gy) * SCALING_FACTOR;
             gz = ((float)raw_gz) * SCALING_FACTOR;
 
-            // Example 2: Apply Simple low-pass filter
+            // Applying Simple low-pass filter
             filtered_gx = FILTER_COEFFICIENT * gx + (1 - FILTER_COEFFICIENT) * filtered_gx;
             filtered_gy = FILTER_COEFFICIENT * gy + (1 - FILTER_COEFFICIENT) * filtered_gy;
             filtered_gz = FILTER_COEFFICIENT * gz + (1 - FILTER_COEFFICIENT) * filtered_gz;
 
-            //printf("RAW -> \t\tgx: %d \t gy: %d \t gz: %d\t\n", raw_gx, raw_gy, raw_gz);
-
-            // printf(">x_axis_low:%4.5f|g\n", filtered_gx);
-            // printf(">y_axis_low:%4.5f|g\n", filtered_gy);
-            // printf(">z_axis_low:%4.5f|g\n", filtered_gz);
-
-            // Example 3: Apply simple high-pass filter with the lpf (by eliminating low freq elements)
-            // to be used with example 2 (together)
+            // Applying simple high-pass filter with the lpf (by eliminating low freq elements)
             high_pass_gx = gx - filtered_gx;
             high_pass_gy = gy - filtered_gy;
             high_pass_gz = gz - filtered_gz;
@@ -208,45 +198,36 @@ int main() {
             printf(">y_axis_high:%4.5f|g\n", high_pass_gy);
             printf(">z_axis_high:%4.5f|g\n", high_pass_gz);
 
-            // record data samples in array
+            // Record data samples in the array according to their axes
             gx_final[i] = high_pass_gx;
             gy_final[i] = high_pass_gy;
             gz_final[i] = high_pass_gz;
-            
-
-            //----Angular Velocity----
-            //float angularVelocityX, angularVelocityY, angularVelocityZ;
-
-            //conv to vel
-            //angularVelocityX = gx_final[i];
-            //angularVelocityY = gy_final[i];
-            //angularVelocityZ = gz_final[i];
-
-            //vel per sec
-            //float scaleFactor = 0.07;  //replace
-            //float angularVelocityX_DPS = angularVelocityX * scaleFactor;
-            //float angularVelocityY_DPS = angularVelocityY * scaleFactor;
-            //float angularVelocityZ_DPS = angularVelocityZ * scaleFactor;
+        
 
 
-            //-----Conversion----
-            //Z axis is used as device will be attached to side
-            Velocity[i] = conv(radius, gz_final[i]);
-            dist(Velocity[i], &Distance);
+            //-----Conversion of Angular Velocities to the Linenar Velocity and Calculating the Distance----
+            //Z axis is used as the device will be calibrated along the Z-axis
+            Velocity[i] = calculateLinearVelocity(radius, gz_final[i]); // Function to calculate the instantaneous velocity
+            calculateLinearDistance(Velocity[i], &Distance); // Function to calculate the instantaneous Distance
 
 
             //increment array 
             i++;
 
-            //sleep for 500ms = 0.5 second readings
+            //sleep for 500ms as we're reading the data for every 0.5 seconds
             thread_sleep_for(500);
             
         }
-        printf("Distance: %f\n", Distance*1.3);
+        //Multiplying the Distance value with ERROR_SCALING_FACTOR to minimize the error rate.
+  
+        float finalDistance = Distance * ERROR_SCALING_FACTOR
+        printf("Distance: %.2f\n", finalDistance);
+
+        // Printing the data to LCD Screen
         led.write(0);
         lcd.ClearStringLine(LINE(8));
         snprintf(display_buf[0], 60, "Distance Travelled:");
-        snprintf(display_buf[1], 60, "%.2f cm", Distance*1.3);
+        snprintf(display_buf[1], 60, "%.2f metres", finalDistance);
         lcd.SelectLayer(FOREGROUND);
         lcd.DisplayStringAt(5, LINE(8), (uint8_t *)display_buf[0], CENTER_MODE);
         lcd.DisplayStringAt(5, LINE(9), (uint8_t *)display_buf[1], CENTER_MODE);
