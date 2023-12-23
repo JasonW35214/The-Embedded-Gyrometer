cb87e4c40ad00585f53c436196d6e7289d71810c
Akash <54092294+akashp04@users.noreply.github.com>
2023-12-22 20:40:10 -0500

    Update conversion.h

--- Embedded_Challenge/include/conversion.h
+++ Embedded_Challenge/include/conversion.h
@@ -6,17 +6,8 @@
 #define dt 0.5
 
 float averageVelocity = 0.0f;
+float calculateLinearVelocity(float omega);
+void calculateLinearDistance(float linearVelocity, float *DISTANCE);
 
-float calculateLinearVelocity(float omega){
-    float omega_mps, linearVelocity; 
-    // omega_mps = omega * (PI / 180); // IF the units of angular velocity are in degrees/second
-    // linearVelocity = RADIUS * omega_mps;
-    linearVelocity = RADIUS * omega;
-    averageVelocity += linearVelocity; 
-    return linearVelocity;
-}
 
-void calculateLinearDistance(float linearVelocity, float *DISTANCE){
-    float instantaneousDistance = abs(linearVelocity) * dt;
-    *DISTANCE += instantaneousDistance;    
-}
+
